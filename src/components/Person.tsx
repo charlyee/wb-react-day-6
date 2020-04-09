916d9558c7e68129b5f81c25a424fd3b5e741a28
Warren Uhrich <hello@warren.codes>
2020-04-09 11:11:01 -0600

    Added 'Update Person' form.

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -23,6 +23,16 @@ export default class Person extends React.Component<IPersonProps, IPersonState> 
             <div>
                 <h2>{this.state.firstName} {this.state.lastName}</h2>
                 <p>They are {this.state.age} years old.</p>
+                <form onSubmit={}>
+                    <h3>Update Person</h3>
+                    <label htmlFor="first-name">First Name:</label>
+                    <input type="text" name="first-name" value={this.state.firstName} />
+                    <label htmlFor="last-name">Last Name:</label>
+                    <input type="text" name="last-name" value={this.state.lastName} />
+                    <label htmlFor="age">Age:</label>
+                    <input type="number" name="age" value={this.state.age} />
+                    <input type="submit" value="Apply Changes" />
+                </form>
             </div>
         );
     }
